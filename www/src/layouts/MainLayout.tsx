03ce841c2a98416cd204d50e37a9b74944ffab29
roblotter <roblotter1@gmail.com>
2021-07-26 15:18:12 +0300

    update

--- www/src/layouts/MainLayout.tsx
+++ www/src/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@
 import { light } from "@www/styles/utils.css";
 import { appClassName } from "./_app.css";
 
-export function MainLayout({ children, className }) {
+export function MainLayout({ children, className = "" }) {
   return (
     <div className={`${className || ""} ${appClassName} ${light}`}>
       {children}
