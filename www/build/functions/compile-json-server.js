2a865501c36a3dcfcd9a723b6f82aa9a22eb5aab
roblotter <roblotter1@gmail.com>
2021-09-01 08:25:10 +0300

    update

---
+++ www/build/functions/compile-json-server.js
@@ -0,0 +1,30 @@
+const watch = process.argv[2] === "--watch";
+
+require("esbuild")
+  .build({
+    entryPoints: ["functions/json-server/fn.js"],
+    bundle: true,
+
+    watch: watch
+      ? {
+          onRebuild(error, result) {
+            if (error) console.error("watch build failed:", error);
+            else console.log("watch build succeeded:", result);
+          },
+        }
+      : null,
+    // external: ['react', 'react-dom'],
+    external: [
+      "express",
+      "serverless-http",
+      "path",
+      "body-parser",
+      "json-server",
+    ],
+    format: "cjs",
+    target: "es2016",
+
+    platform: "node",
+    outfile: `functions/json-server/json-server.js`,
+  })
+  .catch(() => process.exit(1));
--- www/serve-data.js
+++ www/serve-data.js
@@ -1,5 +1,5 @@
 "use strict";
 
-const app = require("./functions/json-server/json-server");
+const app = require("./functions/json-server/fn");
 
 app.listen(3002, () => console.log("Local app listening on port 3002!"));
