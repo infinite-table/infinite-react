7c6e307a41445233314143e70c6b6b352b83b14f
roblotter <roblotter1@gmail.com>
2021-10-13 16:32:09 +0300

    expose useComponentState and release version canary

--- source/src/index.tsx
+++ source/src/index.tsx
@@ -3,3 +3,8 @@ export * from './components/InfiniteTable';
 export * from './components/DataSource';
 
 export { group, flatten } from './utils/groupAndPivot';
+
+export {
+  useComponentState,
+  getComponentStateRoot,
+} from './components/hooks/useComponentState';
