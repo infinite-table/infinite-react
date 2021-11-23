745bdd2532b236ac5dee135592ba70a9b17cc489
roblotter <roblotter1@gmail.com>
2021-11-23 20:39:04 +0200

    update

--- www/functions/json-server/fn.js
+++ www/functions/json-server/fn.js
@@ -1,26 +1,27 @@
-const cors = require("cors");
+const cors = require('cors');
 
-const express = require("express");
-const serverless = require("serverless-http");
+const express = require('express');
+const serverless = require('serverless-http');
 
-const bodyParser = require("body-parser");
-const jsonServer = require("json-server");
+const bodyParser = require('body-parser');
+const jsonServer = require('json-server');
 
-const { employees } = require("../../dataserver/data/employees.json");
 const {
-  employees: employees100k,
-} = require("../../dataserver/data/employees100k.json");
+  employees,
+} = require('../../dataserver/data/employees.json');
+const {
+  employees: employees50k,
+} = require('../../dataserver/data/employees50k.json');
 const router = jsonServer.router({
   employees,
   employees10: employees.slice(0, 10),
   employees100: employees.slice(0, 100),
   employees1k: employees.slice(0, 1000),
   employees10k: employees,
-  employees20k: employees100k.slice(0, 20000),
-  employees30k: employees100k.slice(0, 30000),
-  employees40k: employees100k.slice(0, 40000),
-  employees50k: employees100k.slice(0, 50000),
-  employees100k,
+  employees20k: employees50k.slice(0, 20000),
+  employees30k: employees50k.slice(0, 30000),
+  employees40k: employees50k.slice(0, 40000),
+  employees50k: employees50k.slice(0, 50000),
 });
 
 const app = express();
@@ -29,7 +30,7 @@ const app = express();
 app.use(cors());
 // }
 app.use(bodyParser.json());
-app.use("/.netlify/functions/json-server", router); // path must route to lambda
+app.use('/.netlify/functions/json-server', router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
