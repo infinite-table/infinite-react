d2d8b083575e51d2c4ded1843cc7ce8ec3323df1
roblotter <roblotter1@gmail.com>
2021-11-23 23:09:16 +0200

    update site

--- www/functions/json-server/json-server.js
+++ www/functions/json-server/json-server.js
@@ -10,18 +10,19 @@ const {
   employees,
 } = require('../../dataserver/data/employees.json');
 const {
-  employees: employees50k,
-} = require('../../dataserver/data/employees50k.json');
+  employees: employees100k,
+} = require('../../dataserver/data/employees100k.json');
 const router = jsonServer.router({
   employees,
   employees10: employees.slice(0, 10),
   employees100: employees.slice(0, 100),
   employees1k: employees.slice(0, 1000),
   employees10k: employees,
-  employees20k: employees50k.slice(0, 20000),
-  employees30k: employees50k.slice(0, 30000),
-  employees40k: employees50k.slice(0, 40000),
-  employees50k: employees50k.slice(0, 50000),
+  employees20k: employees100k.slice(0, 20000),
+  employees30k: employees100k.slice(0, 30000),
+  employees40k: employees100k.slice(0, 40000),
+  employees50k: employees100k.slice(0, 50000),
+  employees100k,
 });
 
 const app = express();
--- www/serve-data.js
+++ www/serve-data.js
@@ -1,5 +1,7 @@
-"use strict";
+'use strict';
 
-const app = require("./functions/json-server/fn");
+const app = require('./functions/json-server/json-server');
 
-app.listen(3002, () => console.log("Local app listening on port 3002!"));
+app.listen(3002, () =>
+  console.log('Local app listening on port 3002!')
+);
