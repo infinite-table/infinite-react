5aa27ed3fbe0ff28f16a0abc6cdbe14335d85363
roblotter <roblotter1@gmail.com>
2021-12-01 16:33:24 +0200

    release version patch

--- www/src/components/MDX/Sandpack/index.tsx
+++ www/src/components/MDX/Sandpack/index.tsx
@@ -10,7 +10,7 @@ import { useInfiniteTemplate } from '../../useInfiniteTemplate';
 import { SandpackFiles } from '@codesandbox/sandpack-react/dist/types/types';
 
 const DEPS_VERSIONS: Record<string, string> = {
-  '@infinite-table/infinite-react': '0.0.5',
+  '@infinite-table/infinite-react': '0.0.6',
   'react-query': '3.33.7',
 };
 
