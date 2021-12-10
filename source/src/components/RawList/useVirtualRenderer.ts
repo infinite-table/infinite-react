1e6fd2a0a87c19c9bdd972f4d12ed86131f09f79
roblotter <roblotter1@gmail.com>
2021-12-10 16:38:54 +0200

    fix useVirtualRenderer and try to release version patch

--- source/src/components/RawList/useVirtualRenderer.ts
+++ source/src/components/RawList/useVirtualRenderer.ts
@@ -7,35 +7,19 @@ import { buildSubscriptionCallback } from '../utils/buildSubscriptionCallback';
 import { VirtualBrain } from '../VirtualBrain';
 import { ReactVirtualRenderer } from './ReactVirtualRenderer';
 
-// this was needed for React.StrictMode, so we keep the same renderer
-// for the same brain instance, as in strict mode, hooks are called twice
-// and strict mode behaves differently in various versions of React
-// TODO for future: check if still needed
 function createRenderer(brain: VirtualBrain) {
-  // @ts-ignore
-  if (brain.__rendererResult) {
-    // @ts-ignore
-    return brain.__rendererResult;
-  }
   const renderer = new ReactVirtualRenderer(brain, {});
   const onRenderUpdater = buildSubscriptionCallback<Renderable>();
 
   brain.onDestroy(() => {
-    // @ts-ignore
-    delete brain.__rendererResult;
     renderer.destroy();
     onRenderUpdater.destroy();
   });
 
-  const result = {
+  return {
     renderer,
     onRenderUpdater,
   };
-
-  // @ts-ignore
-  brain.__rendererResult = result;
-
-  return result;
 }
 export function useVirtualRenderer({
   brain,
