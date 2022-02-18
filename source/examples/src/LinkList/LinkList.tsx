f71e6979f39fc6ba3f47c3c43aa755bc440af7a9
roblotter <roblotter1@gmail.com>
2022-02-19 01:06:15 +0200

    release version patch

--- source/examples/src/LinkList/LinkList.tsx
+++ source/examples/src/LinkList/LinkList.tsx
@@ -1,13 +1,7 @@
 import { ReactNode } from 'react';
 
-import styles from './index.module.scss';
-
-console.log(styles);
 export default ({ children }: { children: ReactNode }) => (
-  <div
-    className={styles.linklist}
-    style={{ display: 'flex', flexFlow: 'column', padding: 20 }}
-  >
+  <div style={{ display: 'flex', flexFlow: 'column', padding: 20 }}>
     {children}
   </div>
 );
