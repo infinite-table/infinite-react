b4948a676e55b9698eab10875cf71ee7648c8503
radubrehar <radu@jslog.com>
2023-12-08 18:04:43 +0200

    redeploy site

--- www/src/components/IndexPage.tsx
+++ www/src/components/IndexPage.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Card, Cards, CardsSubtitle } from '@www/components/Cards';
 import { MainContent, MainLayout } from '@www/layouts/MainLayout';
 import * as React from 'react';
