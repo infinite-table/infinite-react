a0f3b195fd3f138abdc29c22e9a1dcf29c253766
radubrehar <radu@jslog.com>
2024-11-19 17:09:25 +0200

    update includes icon

--- source/src/components/InfiniteTable/components/icons/IncludesOperatorIcon.tsx
+++ source/src/components/InfiniteTable/components/icons/IncludesOperatorIcon.tsx
@@ -6,7 +6,9 @@ export const IncludesOperatorIcon = (
 ) => {
   return (
     <Icon {...props}>
-      <path d="M5,4V7H10.5V19H13.5V7H19V4H5Z" />
+      <path d="M11.14 4L6.43 16H8.36L9.32 13.43H14.67L15.64 16H17.57L12.86 4M12 6.29L14.03 11.71H9.96M20" />
+      <path d="M4 18V15H2V20H22V18Z" />
+      <path d="M20 14V18H2V20H22V14Z" />
     </Icon>
   );
 };
