3c183b001af02dc597aa7cfba540700310eb68d8
radubrehar <radu@jslog.com>
2025-03-07 15:24:50 +0200

    minor theme fixes

--- examples/src/pages/_app.page.tsx
+++ examples/src/pages/_app.page.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 import '../index.global.css';
 import '../globals.css';
 
+import '../../../source/dist/theme/default.css';
+import '../../../source/dist/theme/shadcn.css';
+import '../../../source/dist/theme/ocean.css';
+import '../../../source/dist/theme/minimalist.css';
+import '../../../source/dist/theme/balsam.css';
+
 import { InfiniteTable } from '@infinite-table/infinite-react';
 
 InfiniteTable.licenseKey = process.env.NEXT_PUBLIC_INFINITE_LICENSE_KEY;
--- source/src/components/InfiniteTable/vars-common.css.ts
+++ source/src/components/InfiniteTable/vars-common.css.ts
@@ -1,6 +1,8 @@
 import { ThemeVars } from './vars.css';
 
 export const CommonThemeVars = {
+  [ThemeVars.components.Header.background]:
+    ThemeVars.components.HeaderCell.hoverBackground,
   [ThemeVars.components.Cell.flashingBackground]: ThemeVars.color.accent,
   [ThemeVars.components.Cell.flashingUpBackground]: ThemeVars.color.success,
   [ThemeVars.components.Cell.flashingDownBackground]: ThemeVars.color.error,
