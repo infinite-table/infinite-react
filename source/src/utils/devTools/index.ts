81e06000548236a1423b955d4125b0ccce0fe19f
radubrehar <radu@jslog.com>
2025-10-29 12:51:32 +0200

    remove duplicate devtoolstracks definition

--- source/src/utils/devTools/index.ts
+++ source/src/utils/devTools/index.ts
@@ -1,3 +1,4 @@
+import { DevToolsTracks } from './devToolsTracks';
 import { PerfMarker, PerfMarkerDetails } from './PerfMarker';
 
 type DevToolsColor =
@@ -213,39 +214,6 @@ export class DevToolsMarker implements PerfMarker {
     return this;
   };
 }
-
-export const DevToolsTracks = {
-  DataSource: {
-    track: 'DataSource',
-    labels: {
-      RemoteDataLoad: 'Remote data load',
-      TreeUnfilteredTreePaths: 'Computing unfiltered tree paths',
-      Filter: 'Filter',
-      Sort: 'Sort',
-      Group: 'Group',
-      LazyGroup: 'Lazy group',
-      Tree: 'Tree',
-      FlattenTree: 'Flatten tree',
-      Flatten: 'Flatten groups',
-      PrepareData: 'Preparing data',
-      ComputeSelectionCount: 'Computing selection count',
-      ComputeSelectionCountLazy: 'Computing selection count (lazy)',
-      PrepareRowInfo: 'Preparing row info array',
-    },
-  },
-  InfiniteTable: {
-    track: 'InfiniteTable',
-    labels: {
-      Render: 'Rendering',
-    },
-  },
-  MatrixBrain: {
-    track: 'Layout Computations',
-    labels: {
-      ComputeRenderRange: 'Computing render range',
-    },
-  },
-} as const;
 
 export function getMarker(debugId: string) {
   return DevToolsMarker.create(debugId);
